138b071a3d2c822482a73a444ca7b5c6ae0e91c5
code-nuage <eloi.hariot@protonmail.com>
2026-09-17 10:49:06 +0200

    [Green] Parking is paid by slices, with 30 minutes free

--- parking/parking.py
+++ parking/parking.py
@@ -1,2 +1,8 @@
+import math
+
+FREE_MINUTES = 30
+PRICE_PER_HALF_HOUR = 1.50
+
 def final_price(minutes: int) -> float:
-    return 0.00
+    paid_minutes = max(0, minutes - FREE_MINUTES)
+    return math.ceil(paid_minutes / 30) * PRICE_PER_HALF_HOUR
